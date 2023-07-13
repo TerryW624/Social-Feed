97164c1c2a8cfd21cc643da13efb04281e9556dc
TerryW624 <terrywade634@gmail.com>
2023-07-13 11:31:24 -0700

    Setting up components docs

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const FeedComponent = (props) => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default FeedComponent;
+props
